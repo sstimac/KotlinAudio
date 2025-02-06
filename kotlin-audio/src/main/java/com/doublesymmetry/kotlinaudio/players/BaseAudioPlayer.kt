13fad99f122a0d2738822c5bfd5a4d1213f85bbb
sstimac <sstimac.contact@gmail.com>
2025-02-06 14:41:38 +0100

    Revert "Update BaseAudioPlayer.kt"
    This reverts commit c03d0dadc8929bf38d59e53c7229ed5b015aea34.

--- kotlin-audio/src/main/java/com/doublesymmetry/kotlinaudio/players/BaseAudioPlayer.kt
+++ kotlin-audio/src/main/java/com/doublesymmetry/kotlinaudio/players/BaseAudioPlayer.kt
@@ -249,7 +249,6 @@ abstract class BaseAudioPlayer internal constructor(
             }
 
             override fun onPlayFromSearch(query: String?, extras: Bundle?) {
-                Timber.tag("GVATest").d("playing before from query: %s", query)
                 super.onPlayFromSearch(query, extras)
                 Timber.tag("GVATest").d("playing from query: %s", query)
                 mediaSessionCallback.handlePlayFromSearch(query, extras)
